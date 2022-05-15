9417d6e8a56c9a432c89f8cbf47aa8c78e188f47
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-15 20:04:10 -0300

    refactor: repository

--- src/infrastructure/repository/TaskRepository.ts
+++ src/infrastructure/repository/TaskRepository.ts
@@ -2,46 +2,50 @@ import { Task } from "../../entities/Task";
 import { TasRepositoryPort } from "../../usecases/ports/TaskRepository";
 import { Store } from "../store";
 
-const storeInstance = Store.getInstance();
-
 export class TaskRepository implements TasRepositoryPort {
   async find(params = {}): Promise<Task[]> {
     const [firstSearch, ...anotherSerach] = Object.entries(params).map(
       ([key, value]) => `${key}='${value}'`
     );
-    const store = await storeInstance.init();
+    const store = await Store.init();
     const tasks = store
       .objects<Task>("Tasks")
       .filtered(firstSearch, ...anotherSerach);
+    store.close();
     return tasks.map((task) => task);
   }
 
   async findById(id: string): Promise<Task> {
-    const store = await storeInstance.init();
-    return store.objectForPrimaryKey<Task>("Tasks", id);
+    const store = await Store.init();
+    const tasks = store.objectForPrimaryKey<Task>("Tasks", id);
+    store.close();
+    return tasks;
   }
 
   async create(task: Task): Promise<void> {
-    const store = await storeInstance.init();
+    const store = await Store.init();
     store.write(() => {
       store.create("Task", task);
     });
+    store.close();
   }
 
   async update({ id, ...restTaskToUpdate }: Task): Promise<void> {
     const task = await this.findById(id);
-    const store = await storeInstance.init();
+    const store = await Store.init();
     store.write(() => {
       Object.assign(task, restTaskToUpdate);
     });
+    store.close();
   }
 
   async deleteById(id: string): Promise<void> {
     let task = await this.findById(id);
-    const store = await storeInstance.init();
+    const store = await Store.init();
     store.write(() => {
       store.delete(task);
       task = null;
     });
+    store.close();
   }
 }
--- src/infrastructure/store/index.ts
+++ src/infrastructure/store/index.ts
@@ -3,23 +3,7 @@ import { TaskSchema } from "./schemas/Task";
 import { TaskLocalUpdatedSchema } from "./schemas/TaskLocalUpdated";
 
 export class Store {
-  private static instance: Store;
-
-  private realm: Realm;
-
-  private constructor() {}
-
-  static getInstance(): Store {
-    if (!Store.instance) {
-      Store.instance = new Store();
-    }
-    return Store.instance;
-  }
-
-  async init() {
-    if (!!this.realm) {
-      return this.realm;
-    }
+  static async init() {
     return Realm.open({
       path: "mylocalDB",
       schema: [TaskSchema, TaskLocalUpdatedSchema],
