91a3413f4c3ca9fadba1159b4e3902e1a89f2817
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-18 12:38:13 -0300

    feat: home page

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,12 +1,53 @@
-import React from "react";
+import BottomSheet from "@gorhom/bottom-sheet";
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 import { ButtonCreate } from "../../components/Buttons/Create";
 import { Container } from "./styles";
 
 export const Home = (): JSX.Element => {
+  const [isVisible, setIsVisible] = useState(false);
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ["25%", "50%"], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  const onHandleCreate = () => {
+    setIsVisible(true);
+  };
+
   return (
     <Container>
-      <ButtonCreate />
+      <ButtonCreate onPress={onHandleCreate} />
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={isVisible ? 1 : -1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
