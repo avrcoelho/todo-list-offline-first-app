6da029bc2a3edd3847a43975ed2678d2cee5595e
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-20 10:44:35 -0300

    feat: controlled input

---
+++ src/presentation/components/Inputs/TextControlled/index.tsx
@@ -0,0 +1,24 @@
+import { TextInputProps } from "react-native";
+import { Control, Controller } from "react-hook-form";
+import { InputText } from "../Text";
+
+type InputTextControlledProps = Omit<TextInputProps, "accessibilityRole"> & {
+  name: string;
+  control: Control<any>;
+  error?: string;
+  label?: string;
+};
+
+export const InputTextControlled = ({
+  control,
+  name,
+  ...restProps
+}: InputTextControlledProps) => {
+  <Controller
+    name={name}
+    control={control}
+    render={({ field: { onChange, value } }) => (
+      <InputText {...restProps} onChange={onChange} value={value} />
+    )}
+  />;
+};
