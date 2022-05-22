6bd7f08c4cdf61551db0b63b103a90b02d83b6b0
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-22 08:57:49 -0300

    feat: loader footer list component

---
+++ src/presentation/components/Loaders/FooterList/index.tsx
@@ -0,0 +1,23 @@
+import { ActivityIndicator } from "react-native";
+
+import { Container } from "./styles";
+
+interface LoaderFooterListProps {
+  isLoading: boolean;
+}
+
+export const LoaderFooterList = ({
+  isLoading,
+}: LoaderFooterListProps): JSX.Element => {
+  return (
+    <Container>
+      {isLoading && (
+        <ActivityIndicator
+          size="small"
+          color="#001e2b"
+          testID="loader-footer-list"
+        />
+      )}
+    </Container>
+  );
+};
---
+++ src/presentation/components/Loaders/FooterList/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 80px;
+  justify-content: center;
+  align-items: center;
+`;
