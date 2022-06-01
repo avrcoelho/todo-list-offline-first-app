f4b3b2de48efbc4a9d1d927891f488506f82f78d
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-31 21:26:12 -0300

    feat: return task data after create

--- App.tsx
+++ App.tsx
@@ -3,15 +3,18 @@ import "react-native-get-random-values";
 import { SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NotificationContainer } from "react-native-hook-notification";
+import { isIphoneX } from "react-native-iphone-x-helper";
 
 import { Home } from "./src/presentation/pages/Home";
+
+const X_OFFSET = isIphoneX() ? 30 : 10;
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style="auto" />
       <Home />
-      <NotificationContainer xOffset={30} position="bottom-center" />
+      <NotificationContainer xOffset={X_OFFSET} position="bottom-center" />
     </SafeAreaView>
   );
 }
