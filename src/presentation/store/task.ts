95f80f73d5aebf0725447b524b70c0395c6a9b8e
avrcoelho <andrevrcoelho@hotmail.com>
2022-06-02 13:17:41 -0300

    feat: update store to update task

--- src/presentation/store/task.ts
+++ src/presentation/store/task.ts
@@ -4,14 +4,18 @@ import { Task } from "../../entities/Task";
 
 type UseTaskStore = {
   tasks: Task[];
+  taskIdToUpdate: null | string;
   init(tasks: Task[]): void;
   add(task: Task): void;
   update(task: Task): void;
   remove(_id: string): void;
+  addTaskIdToUpdate(_id: string): void;
+  removeTaskIdToUpdate(): void;
 };
 
 export const useTaskStore = create<UseTaskStore>((set) => ({
   tasks: [],
+  taskIdToUpdate: null,
   init: (tasks) => set({ tasks }),
   add: (task) => set((state) => ({ ...state, tasks: [...state.tasks, task] })),
   update: (task) =>
@@ -23,5 +27,15 @@ export const useTaskStore = create<UseTaskStore>((set) => ({
     set((state) => ({
       ...state,
       tasks: state.tasks.filter((task) => task._id !== _id),
+    })),
+  addTaskIdToUpdate: (_id) =>
+    set((state) => ({
+      ...state,
+      taskIdToUpdate: _id,
+    })),
+  removeTaskIdToUpdate: () =>
+    set((state) => ({
+      ...state,
+      taskIdToUpdate: null,
     })),
 }));
