3267fb6311c008fa61e54612c3ebe235b10c8012
avrcoelho <andrevrcoelho@hotmail.com>
2022-06-04 12:48:15 -0300

    refactor: update task factory

--- src/main/factories/usecases/updateTask.ts
+++ src/main/factories/usecases/updateTask.ts
@@ -1,7 +1,9 @@
+import { Task } from "../../../entities/Task";
 import { TaskRepository } from "../../../infrastructure/repository/TaskRepository";
 import { UpdateTask } from "../../../usecases/UpdateTask";
 
-export const makeUpdateTask = () => {
+export const makeUpdateTask = (task: Task) => {
   const taskRepository = new TaskRepository();
-  return new UpdateTask(taskRepository);
+  const updateTask = new UpdateTask(taskRepository);
+  updateTask.execute.call(updateTask, task);
 };
