52598e4b612ad8b13f2ba1150f2565fbde4aa5ae
avrcoelho <andrevrcoelho@hotmail.com>
2022-06-06 19:02:31 -0300

    feat: TaskTo Sync  repom

---
+++ src/infrastructure/repository/TaskToSyncRepository.ts
@@ -0,0 +1,36 @@
+import { TaskToSync } from "../../entities/TaskToSync";
+import { TaskToSyncRepositoryPort } from "../../usecases/ports/TaskToSyncRepository";
+import { Store } from "../store";
+
+export class TaskToSyncRepository implements TaskToSyncRepositoryPort {
+  async find(): Promise<TaskToSync[]> {
+    const store = await Store.init();
+    let tasks = store.objects<TaskToSync>("TaskToSync");
+    return tasks.map((task) => ({
+      _id: task._id,
+      type: task.type,
+    }));
+  }
+
+  async findById(_id: string): Promise<TaskToSync | undefined> {
+    const store = await Store.init();
+    const _idParsed = new Realm.BSON.ObjectId(_id);
+    return store.objectForPrimaryKey<TaskToSync>("TaskToSync", _idParsed);
+  }
+
+  async create(taskToSync: TaskToSync): Promise<void> {
+    const store = await Store.init();
+    store.write(() => {
+      store.create("TaskToSync", taskToSync);
+    });
+  }
+
+  async deleteById(id: string): Promise<void> {
+    let taskToSync = await this.findById(id);
+    const store = await Store.init();
+    store.write(async () => {
+      store.delete(taskToSync);
+      taskToSync = undefined;
+    });
+  }
+}
