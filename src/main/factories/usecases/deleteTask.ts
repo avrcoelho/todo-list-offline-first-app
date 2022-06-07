164482ce7ae9b63aef3d58097ffb4f4f4748c0fe
avrcoelho <andrevrcoelho@hotmail.com>
2022-06-07 20:47:18 -0300

    feat: delete task

--- src/main/factories/usecases/deleteTask.ts
+++ src/main/factories/usecases/deleteTask.ts
@@ -1,8 +1,10 @@
 import { TaskRepository } from "../../../infrastructure/repository/TaskRepository";
+import { TaskToSyncRepository } from "../../../infrastructure/repository/TaskToSyncRepository";
 import { DeleteTask } from "../../../usecases/DeleteTask";
 
 export const makeDeleteTask = async (id: string) => {
   const taskRepository = new TaskRepository();
-  const deleteTask = new DeleteTask(taskRepository);
+  const taskToSyncRepository = new TaskToSyncRepository();
+  const deleteTask = new DeleteTask(taskRepository, taskToSyncRepository);
   await deleteTask.execute.call(deleteTask, id);
 };
--- src/usecases/DeleteTask.ts
+++ src/usecases/DeleteTask.ts
@@ -1,9 +1,14 @@
-import { TaskRepository } from "../infrastructure/repository/TaskRepository";
+import { TaskRepositoryPort } from "./ports/TaskRepository";
+import { TaskToSyncRepositoryPort } from "./ports/TaskToSyncRepository";
 
 export class DeleteTask {
-  constructor(private readonly taskRepository: TaskRepository) {}
+  constructor(
+    private readonly taskRepository: TaskRepositoryPort,
+    private readonly taskToSyncRepository: TaskToSyncRepositoryPort
+  ) {}
 
   async execute(_id: string) {
     await this.taskRepository.deleteById(_id);
+    await this.taskToSyncRepository.deleteById(_id);
   }
 }
